19620d8ab0a415408d97a96ff89700f9ab06e0b5
Matt Albrinck <matthewalbrinck@gmail.com>
2019-02-11 12:33:54 -0500

    test commit

--- hw_broken.cc
+++ hw_broken.cc
@@ -1,24 +1,37 @@
+/** \file */
 #include <iostream>
 #include <vector>
 #include <cmath>
+/**
+*	/brief This funciton calculates the standard deviation for a preset array of integers
+*	/param a : This is the array of values
+*	/param n : This is the variables for the numerical values in the array
+*	/return Returns the Standard Deviation
+*/
 
 
-
-int deviation( int* a, int n )
-{
-	int sum;
-	for(size_t i = 0; i <= v.size(); i++)
+double deviation( int* a, int n )
+{	//initializes the sum to be 0
+	double sum=0;
+	//for loop that adds up all of the data in the array
+	for(size_t i = 0; i <= n-1; i++)
 	{
-		sum += v[i];
-	} 
-	double mean = sum /= v.size();
-	double stddev = 0;
-	for(size_t i = 0; i <= v.size() -1; i++)
-	{
-		double stddev = stddev + (v[i] - mean) * (v[i] - mean); 
+		sum += a[i];
 	}
-	stddev /= v.size();
-	if( stddev = 0)
+	//calculates the mean(average)
+	double mean = sum /= n;
+	//initializes the standard deviation to be 0
+	double stddev = 0;
+	//calculates the bulk of the standard deviation
+	for(size_t i = 0; i <= n - 1; i++)
+	{
+		stddev += (a[i] - mean) * (a[i] - mean); 
+	}
+	//statement to see if there is an error and the standard deviation is 0
+	if( stddev == 0)
 		std::cout << "Sigma is zero." << std::endl;
-	return sqrt(stddev);
+	//sets value to return the final standard deviation
+	double final = sqrt(stddev/n);
+	//returns the answer to the calculation
+	return final;
 }
